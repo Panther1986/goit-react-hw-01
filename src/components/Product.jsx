4a2527a887df26c929c2795f34f44feaf963c3f4
Panther1986 <polzovatel77@gmail.com>
2024-02-13 00:02:16 +0200

    update2

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,12 @@
 // src/components/Product.jsx
 
-export const Product = () => {
+export const Product = ({name, imgUrl, price}) => {
   return (
-    <>
-      <h2>Tacos</h2>
-			<img src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>" alt="Tacos With Lime" width="640" />
-      <p>Price: 999 credits</p>
-    </>
+    <div>
+      <h2>{name}</h2>
+			<img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <p>Price: {price} credits</p>
+    </div>
   );
 };
 
